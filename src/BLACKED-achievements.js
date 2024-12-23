7b25809cfd7735258e54afff8ce8c7c4a4c88428
chloeana <therealchloeprice@gmail.com>
2024-12-23 02:37:34 -0500

    outfits, achievements, etc

---
+++ src/BLACKED-achievements.js
@@ -0,0 +1,254 @@
+achievements("BLACKED",
+    {
+      "name": "Moby Black Dick",
+      "hint": "Find the mythical w̶h̶i̶t̶e̶  black whale.",
+      "flavor":
+        '(twirl:"I try all things, I achieve what I can.", "See how elastic our prejudices grow when once love comes to bend them.", "It is the easiest thing in the world for a man to look as if he had a great secret in him.")',
+      "condition_name": "moby-dick-blacked",
+      "visible": "1",
+      "emoji": "🖤🍆"
+    },
+    {
+      "name": "BLACKED from the Start",
+      "hint": "Lose your virginity to a black man.",
+      "flavor":
+        `(twirl:"Bold choices make for bold memories.", "A tale of firsts and fine tastes!", "Starting off strong, aren't we?")`,
+      "condition_name": "virginity-blacked",
+      "visible": "1",
+      "emoji": "💮"
+    },
+    {
+      "name": "Once you go Black...",
+      "hint": "Have your first sexual encounter with a black man.",
+      "flavor":
+        '(twirl:"This could be the beginning of something big.", "Chloeana commends your effort!", "There’s no going back!")',
+      "condition_name": "first-sex-blacked",
+      "visible": "1",
+      "emoji": "🖤"
+    },
+    {
+      "name": "Mud Shark",
+      "hint": "Have sex with 3 different black men at the beach, in a single day.",
+      "flavor":
+        '(twirl:"Swimming with the sharks now!", "One, two, three, make it a spree!", "The beach never saw this coming!")',
+      "condition_name": "mud-shark-blacked",
+      "visible": "1",
+      "emoji": "🦈"
+    },
+    {
+      "name": "Black Baby Maker",
+      "hint": "Get impregnated by a black man.",
+      "flavor":
+        '(twirl:"Building bridges, one family at a time.", "After fully mixed, bake at 350 for 9 months.", "You make such cute babies!")',
+      "condition_name": "bbc-pregnancy-blacked",
+      "visible": "1",
+      "emoji": "👶🏾"
+    },
+    {
+      "name": "Harlem Globetrotters",
+      "hint": "Do your part towards building utopia by having at least five Black babies in a single playthrough.",
+      "flavor":
+        '(twirl:"Really? An entire basketball team?", "We’re building a legacy here.", "Making history, one game at a time.")',
+      "condition_name": "globetrotters-blacked",
+      "visible": "1",
+      "emoji": "🏀"
+    },
+    {
+      "name": "New Black Daddy",
+      "hint": "Introduce an older black fuckbuddy (40+) to your step-dad.",
+      "flavor":
+        '(twirl:"Meet the new man of the house!", "Two daddies are better than one.", "Family gatherings just got more interesting.")',
+      "condition_name": "new-daddy-blacked",
+      "visible": "1",
+      "emoji": "👴🏿"
+    },
+    {
+      "name": "Hood Bicycle",
+      "hint": "Give your number out to five black guys at once.",
+      "flavor":
+        '(twirl:"Everyone in da hood gets a ride.", "Take a number, boys!", "No need for a line, everyone can ride.")',
+      "condition_name": "bbc-bicycle-blacked",
+      "visible": "1",
+      "emoji": "🚴🏿‍♂️"
+    },
+    {
+      "name": "BLACKED Branded",
+      "hint": "Purchase a certain brand's outfit.",
+      "flavor":
+        `(twirl:"Stylin' and profilin'!", "Welcome to the exclusive club.", "Only the best for you!")`,
+      "condition_name": "outfit-blacked",
+      "visible": "1",
+      "emoji": "👙💅"
+    },
+    {
+      "name": "BLACKED Besties",
+      "hint": "Party with Gabbie, Maurice, and Dredd 10 times in a single playthrough.",
+      "flavor":
+        '(twirl:"Nothing like friends who understand you!", "Friends who slay together, stay together.", "Life’s better when you’ve got your blacked bestie in it.")',
+      "condition_name": "besties-blacked",
+      "visible": "1",
+      "emoji": "👭🏼"
+    },
+    {
+      "name": "Stripper?!? Dancing is an art!",
+      "hint": "Strip for Dredd's boys at least 10 times in a single playthrough.",
+      "flavor":
+        '(twirl:"Grace in every move.", "When the art takes over...", "It’s a performance, really.")',
+      "condition_name": "stripper-blacked",
+      "visible": "1",
+      "emoji": "🤑💃"
+    },
+    {
+      "name": "BBC Lover",
+      "hint": "Sleep with 25 black men.",
+      "flavor":
+        '(twirl:"BBC enthusiast confirmed!", "Keeping a record, are we?", "There’s no going back now.")',
+      "condition_name": "sex25-blacked",
+      "visible": "1",
+      "emoji": "🥰"
+    },
+    {
+      "name": "Snowbunny",
+      "hint": "Sleep with 50 black men.",
+      "flavor":
+        '(twirl:"Hopping from one to the next!", "Life’s a wonderland.", "You know what you like!")',
+      "condition_name": "sex50-blacked",
+      "visible": "1",
+      "emoji": "❄️🐇"
+    },
+    {
+      "name": "Queen of Spades",
+      "hint": "Sleep with 100 black men!",
+      "flavor":
+        '(twirl:"A well-earned crown.", "You’ve reached the peak!", "Long may you reign!")',
+      "condition_name": "sex100-blacked",
+      "visible": "1",
+      "emoji": "👸🏼"
+    },
+    {
+      "name": "BBC Nympho",
+      "hint": "Sleep with 10 black men in a single day.",
+      "flavor":
+        '(twirl:"It’s a marathon, not a sprint.", "Going the extra mile... ten times.", "Such a memorable day... and night.")',
+      "condition_name": "sex10-blacked",
+      "visible": "1",
+      "emoji": "⏳💦"
+    },
+    {
+      "name": "Late Night Binge",
+      "hint": "Watch 10 BBC porn scenes in a single night.",
+      "flavor":
+        '(twirl:"No sleep, just gooning.", "Just one more…", "Can’t stop, won’t stop.")',
+      "condition_name": "gooner-marathon-blacked",
+      "visible": "1",
+      "emoji": "📼👀"
+    },
+    {
+      "name": "Gooner",
+      "hint": "Watch 50 BBC porn scenes.",
+      "flavor":
+        '(twirl:"Gooner? That’s a good thing, right?", "In for the long haul!", "You know what you like.")',
+      "condition_name": "gooner-blacked",
+      "visible": "1",
+      "emoji": "🌽"
+    },
+    {
+      "name": "Down the Hatch",
+      "hint": "Swallow 25 loads from black men.",
+      "flavor":
+        '(twirl:"Keeping up with demand.", "A practiced skill.", "Truly in the zone.")',
+      "condition_name": "swallow25-blacked",
+      "visible": "1",
+      "emoji": "🥄"
+    },
+    {
+      "name": "Taste Test Champion",
+      "hint": "Swallow 50 loads from black men.",
+      "flavor":
+        '(twirl:"Consistency is key.", "The dedication shows.", "Nothing but the best.")',
+      "condition_name": "swallow50-blacked",
+      "visible": "1",
+      "emoji": "🍶"
+    },
+    {
+      "name": "Semen Reservoir",
+      "hint": "Swallow 100 loads from black men!",
+      "flavor":
+        '(twirl:"A hundred memories.", "Pure dedication.", "Untiring enthusiasm.")',
+      "condition_name": "swallow100-blacked",
+      "visible": "1",
+      "emoji": "🌊"
+    },
+    {
+      "name": "Swipe Left, All Day",
+      "hint": "Reject 10 white boys in a single day.",
+      "flavor":
+        '(twirl:"When you know what you want.", "Keeping standards high.", "Busy day for turn-downs.")',
+      "condition_name": "reject10-blacked",
+      "visible": "1",
+      "emoji": "📱👈"
+    },
+    {
+      "name": "Eww, no",
+      "hint": "Reject 25 white boys.",
+      "flavor":
+        '(twirl:"Hard pass!", "Not what I’m looking for, honey.", "Nice try, but not my type.")',
+      "condition_name": "reject25-blacked",
+      "visible": "1",
+      "emoji": "🙅‍♀️"
+    },
+    {
+      "name": "Nope, not today",
+      "hint": "Reject 50 white boys.",
+      "flavor":
+        '(twirl:"Maybe another lifetime.", "Some things just aren’t meant to be.", "I know what I want, and it’s not you.")',
+      "condition_name": "reject50-blacked",
+      "visible": "1",
+      "emoji": "🚫"
+    },
+    {
+      "name": "You're out of his league",
+      "hint": "Reject 100 white boys!",
+      "flavor":
+        '(twirl:"Waaayy out of his league.", "Why do they even try?", "Some bars are just too high.")',
+      "condition_name": "reject100-blacked",
+      "visible": "1",
+      "emoji": "👑"
+    },
+    {
+      "name": "...keeps the doctor away",
+      "hint": "Swallow 5 loads from black men in a single day.",
+      "flavor":
+        '(twirl:"A busy day indeed.", "More than just a taste.", "A memorable day.")',
+      "condition_name": "swallow5-blacked",
+      "visible": "0",
+      "emoji": "🍹"
+    },
+    {
+      "name": "BBC Size Queen",
+      "hint": "Get penetrated by so many BBC that you start to become loose and *require* big black cocks to sastify you.",
+      "flavor":
+        '(twirl:"Bigger is better!", "Never settle for anything less...than 8 inches", "High standards, huh?")',
+      "condition_name": "size-queen-blacked",
+      "visible": "0",
+      "emoji": "📏"
+    },
+    {
+      "name": "Asking for it!",
+      "hint": "Take 3 creampies, from BBC, while on a breeder... in a single day.",
+      "flavor":
+        '(twirl:"Playing with fire… and loving it.", "Definitely pushing limits.", "You knew what you were getting into.")',
+      "condition_name": "breeder-blacked",
+      "visible": "0",
+      "emoji": "💦🥧"
+    },
+    {
+      "name": "Brand Affiliate",
+      "hint": "Earn a certain brand's secret outfit.",
+      "flavor":
+        '(twirl:"Wearing it loud and proud.", "Proof that loyalty has its rewards.", "Exclusive threads for exclusive tastes.")',
+      "condition_name": "outfit-secret-blacked",
+      "visible": "0",
+      "emoji": "👙🔥"
+    }
+)
